1ccd1b680b08fa0311e8e6a155b6d1821ff240f5
Tarsem Singh <tarsemsinghtarsemsingh@gmail.com>
2023-02-19 12:45:37 +0530

    Launch browsers and other softwares in windows

--- open_sites_in_browser.py
+++ open_sites_in_browser.py
@@ -1,7 +1,6 @@
 import webbrowser
 import os
 
-
 website_1 = "https://website.in/"
 email = "https://gmail.in/"
 software_name = '"C:\\Users\\AK\\AppData\\Roaming\\software name\\software_name.exe"'
@@ -9,7 +8,6 @@ website_2 = "https://login_here.in/NewLayoutLogin.aspx"
 
 mozilla_path = "C:\Program Files (x86)\Mozilla Firefox\firefox.exe"
 webbrowser.register('firefox', None,webbrowser.BackgroundBrowser(mozilla_path))
-
 
 #open website_1 in chrome browser
 webbrowser.open_new_tab(website_1)
